ed94b60e979fefae46f74c5e9a8ed5e8241e1d9a
Adam Owada <adamowada@gmail.com>
2023-08-23 17:30:17 -0700

    added script for cron job

---
+++ run_grade_reporter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Load the environment variables
+source /home/adamo/projects/codefellows/canvas-grade-reporter/.env
+
+# Run the Python script
+/home/adamo/projects/codefellows/canvas-grade-reporter/.venv/bin/python /home/adamo/projects/codefellows/canvas-grade-reporter/main.py
